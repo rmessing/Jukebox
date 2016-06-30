5c4531e27f5f6a5b305a86eb81e32726e638df9a
Richard Messing <rsmessing@gmail.com>
2016-06-30 00:27:14 -0400

    Commented code and removed unnecessary files

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
 
 var target = document.getElementById('id1');
 
+// Jukebox is a class object with audio control and playlist methods
 function Jukebox() {
 	var index = 0;
 	this.playlist = [];
+// add song to playlist array
 	this.add = function(newSong){
 		this.playlist.push(newSong)
 	}
+// play mp3 and display title, artist, url from the playlist array at index
 	this.play_it = function(){
 		target.setAttribute('src', this.playlist[index].url)
 		target.play();
@@ -15,18 +18,21 @@ function Jukebox() {
 		document.getElementById("a-out").innerHTML = this.playlist[index].artist;
 		document.getElementById("u-out").innerHTML = this.playlist[index].url;
 	}
+// pause song
 	this.pause_it = function(){
 		target.pause();
 	}
+// stop song
 	this.stop_it = function(){
 		target.load();
 	}
+// grab title, artist and url data entry an add to playlist array
 	this.upload = function() {
 		newSong = new Music(document.getElementById('title').value, document.getElementById('artist').value ,document.getElementById('url').value);
-		// console.log("upload ran");
 		// console.log(jimi.playlist);
 		this.add(newSong);
 	}
+// play previous song
 	this.back = function(){
 		index--
 		// console.log(index + "back")
@@ -34,15 +40,13 @@ function Jukebox() {
 			 index = this.playlist.length - 1
 		}	
 		jimi.play_it();
-		// console.log(index + "lllllll")
 	}
+// play next song
 	this.next = function(){
-		// console.log(index)
 		index++
-				// console.log(index + "next")
 		if (index == this.playlist.length) {
-					// console.log("is if triggered")
-					  index = 0
+		// console.log("is if triggered")
+			index = 0
 		}
 		jimi.play_it()
 	}
@@ -52,7 +56,9 @@ function Music(title, artist, url){
 	this.artist = artist;
 	this.url = url;
 }
+// jimi is an instance Jukebox object
 var jimi = new Jukebox();
+// listening for clicks on audio control buttons
 $('#play').on('click', function(){
 		  jimi.play_it()
 });
@@ -71,10 +77,11 @@ document.getElementById("next").onclick = function(){
 document.getElementById("add_song").onclick = function(){
 	jimi.upload();
 }
+// two hard coded songs in playlist 
 var music1 = new Music("Mother's Day Again",'Marty McVeigh',"https://ia800202.us.archive.org/8/items/MothersDayAgain/2016-03-20%20mothers%20day%20again.mp3");
 var music2 = new Music("My Baby Done Left Me", "Marty McVeigh", "http://ia601404.us.archive.org/28/items/MyBabyDoneLeftMe/MyBabyDoneLeftMe.mp3"); 
 jimi.add(music1);
 jimi.add(music2);
-// console.log(jimi.playlist)
+
 });
 
